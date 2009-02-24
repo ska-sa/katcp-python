8118cb04d66e6f3ebbdc8844c2b4f712c46e9a13
simon <simon@b56c7a7d-ac47-0410-a905-b6f7de78896a>
2009-02-24 11:13:21 +0000

    Add test_kattypes to tests run. Activate test_katcp_bnf if ply is available.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk@722 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- test/__init__.py
+++ test/__init__.py
@@ -1,12 +1,21 @@
 import unittest
 import katcp # for module dependencies
-#import test_katcp_bnf
 import test_katcp
+import test_kattypes
+
+try:
+    # BNF tests reply on PLY (Python Lexx/Yacc)
+    import ply
+    import test_katcp_bnf
+except ImportError:
+    ply = None
 
 def suite():
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
-    #suite.addTests(loader.loadTestsFromModule(test_katcp_bnf))
     suite.addTests(loader.loadTestsFromModule(test_katcp))
+    suite.addTests(loader.loadTestsFromModule(test_kattypes))
+    if ply is not None:
+        suite.addTests(loader.loadTestsFromModule(test_katcp_bnf))
     return suite
 
