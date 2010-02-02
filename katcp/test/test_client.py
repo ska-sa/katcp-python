f9450e6c7200a6a2e1b920ec87547d02daff0a1c
simon <simon@b56c7a7d-ac47-0410-a905-b6f7de78896a>
2010-02-02 16:00:08 +0000

    Lengthen sleep time on test.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk@4416 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- katcp/test/test_client.py
+++ katcp/test/test_client.py
@@ -415,7 +415,7 @@ class TestCallbackClient(unittest.TestCase, TestUtilMixin):
             inform_cb=help_inform,
         )
 
-        time.sleep(0.1)
+        time.sleep(0.2)
         self.assertEqual(len(help_replies), 1)
         self.assertEqual(len(help_informs), 13)
 
