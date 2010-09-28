e176107a3acd391c3b1b33341496766cea84360c
fijal <fijal@b56c7a7d-ac47-0410-a905-b6f7de78896a>
2010-09-28 12:08:08 +0000

    provide some sort of an "official API"
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk@6876 b56c7a7d-ac47-0410-a905-b6f7de78896a

---
+++ katcp/tx/__init__.py
@@ -0,0 +1,7 @@
+
+""" A twisted katcp implementation. Interface provided here is an official
+API
+"""
+
+from katcp.tx.core import DeviceServer, DeviceProtocol, ClientKatCP
+from katcp.tx.proxy import DeviceHandler, ProxyProtocol, ProxyKatCP
