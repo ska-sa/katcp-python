610945448ae3e928d4716775944889bfe71d2d0f
simon <simon@b56c7a7d-ac47-0410-a905-b6f7de78896a>
2010-10-25 11:38:04 +0000

    Remove completed todo for renaming katcp.katcp to katcp.core (this was done some time back).
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk@7506 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- katcp/core.py
+++ katcp/core.py
@@ -4,9 +4,6 @@
 # Copyright 2009 SKA South Africa (http://ska.ac.za/)
 # BSD license - see COPYING for details
 
-# XXX note: this file's name prevents it from importing katcp from this
-# directory, can we rename it?
-
 """Utilities for dealing with KAT device control
 language messages.
 """
