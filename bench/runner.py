eaab79aae9e7cb3bdaa0853da73bf16d6c532819
fijal <fijal@b56c7a7d-ac47-0410-a905-b6f7de78896a>
2010-10-27 08:16:07 +0000

    Refactor a bit runner to allow consecutive runs
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk@7588 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- bench/runner.py
+++ bench/runner.py
@@ -6,14 +6,33 @@ from optparse import OptionParser
 from twisted.python import log
 import re, os, sys, signal
 
+BASE_PORT = 1235
+
 class Master(object):
-    def __init__(self, no_of_clients):
-        self.no_of_clients = no_of_clients
+    def __init__(self, no_of_clients, python, servname):
+        self.current_iteration = 1
+        self.python = python
+        self.servname = servname
+        self.max_clients = no_of_clients
         self.count = 0
         self.clients = []
+        self.totals = []
+        self.current_runs = 0
+        self.max_current_runs = 5
 
     def notify_server_lost(self):
-        reactor.stop()
+        if self.current_iteration != self.max_clients:
+            if self.current_runs != self.max_current_runs:
+                self.current_runs += 1
+            else:
+                self.current_runs = 0
+                self.current_iteration += 1
+            self.clients = []
+            self.run()
+            print "next run"
+        else:
+            print self.totals
+            reactor.stop()
 
     def notify_connection_made(self, pid):
         self.clients.append(pid)
@@ -22,12 +41,22 @@ class Master(object):
         self.no_of_clients -= 1
         self.count += info
         if self.no_of_clients == 0:
-            print "Total: %d" % self.count
+            self.totals.append((self.count, self.current_iteration))
+            self.count = 0
             os.kill(self.server.transport.pid, signal.SIGTERM)
 
     def stop(self):
         for pid in self.clients:
             os.kill(pid, signal.SIGTERM)
+
+    def run(self):
+        self.no_of_clients = self.current_iteration
+        port = str(BASE_PORT + self.current_iteration)
+        self.server = BenchmarkServer(self.python, self.current_iteration, self,
+                                      port)
+        reactor.spawnProcess(self.server, self.python,
+                             args=[self.python, self.servname,
+                                   '--port', port])
 
 class BenchmarkClient(ProcessProtocol):
     id = 0
@@ -61,20 +90,21 @@ class BenchmarkClient(ProcessProtocol):
         self.sensors = []
 
 class BenchmarkServer(ProcessProtocol):
-    def __init__(self, python, options, master):
-        self.options = options
-        self.python  = python
-        self.master  = master
+    def __init__(self, python, no_of_clients, master, port):
+        self.no_of_clients = no_of_clients
+        self.python        = python
+        self.master        = master
+        self.port          = port
 
     def outReceived(self, out):
         reactor.spawnProcess(BenchmarkClient(self.master), self.python,
                              args=[self.python, 'benchtxclient1.py',
-                                   '--port', '1235', '--allow-sensor-creation'])
-        for i in range(self.options.no_of_clients - 1):
+                                   '--port', self.port, '--allow-sensor-creation'])
+        for i in range(self.no_of_clients - 1):
             reactor.spawnProcess(BenchmarkClient(self.master),
                                  self.python,
                                  args=[self.python, 'benchtxclient1.py',
-                                       '--port', '1235'])
+                                       '--port', self.port])
 
     def processEnded(self, status):
         self.master.notify_server_lost()
@@ -97,16 +127,12 @@ def main(python=sys.executable):
         options.scenarios = [1, 2]
     else:
         options.scenarios = [int(i) for i in options.scenarios.split(',')]
-    master = Master(options.no_of_clients)
-    server = BenchmarkServer(python, options, master)
-    master.server = server
     if options.twisted:
         servname = 'benchtxserver.py'
     else:
         servname = 'benchserver.py'
-    reactor.spawnProcess(server, python,
-                         args=[python, servname,
-                               '--port', '1235']) # or normal
+    master = Master(options.no_of_clients, python, servname)
+    master.run()
     reactor.run()
 
 if __name__ == '__main__':
