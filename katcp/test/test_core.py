52f8f1bfa40eeab18bb17ca8e62267e8c59bbb65
nmarais <nmarais@b56c7a7d-ac47-0410-a905-b6f7de78896a>
2013-05-31 09:44:23 +0000

    Cleanup some merging nonsense
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk@17802 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- katcp/test/test_core.py
+++ katcp/test/test_core.py
@@ -318,19 +318,6 @@ class TestSensor(unittest.TestCase):
                            default=('192.168.101.1', 81))
         self.assertEqual(s._value, ('192.168.101.1', 81))
 
-    def test_address_sensor(self):
-        """Test address sensor."""
-        s = Sensor.address("a.address", "An address sensor.", "", None)
-        self.assertEqual(s.stype, 'address')
-        s.set(timestamp=12345, status=Sensor.NOMINAL, value=("127.0.0.1", 80))
-        self.assertEqual(s.read_formatted(),
-                         ("12345.000000", "nominal", "127.0.0.1:80"))
-        self.assertEqual(s.parse_value("[::1]:80"), ("::1", 80))
-        self.assertRaises(ValueError, s.parse_value, "[::1]:foo")
-        s = Sensor.address("a.address", "An address sensor.", "",
-                           default=('192.168.101.1', 81))
-        self.assertEqual(s._value, ('192.168.101.1', 81))
-
     def test_set_and_get_value(self):
         """Test getting and setting a sensor value."""
         s = Sensor.integer("an.int", "An integer.", "count", [-4, 3])
