75cec9fa2b901b935a473f2058845fc16add1898
nmarais <nmarais@b56c7a7d-ac47-0410-a905-b6f7de78896a>
2014-03-14 10:00:42 +0000

    (nmarais, charlesdv) Add utility code to version.py that other modules can use to parse svn or git revisions for use in build-state informs or sensors
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk@20590 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- katcp/version.py
+++ katcp/version.py
@@ -7,6 +7,8 @@
 """katcp version information.
    """
 
+import pkg_resources
+
 VERSION = (0, 5, 5, 'alpha', 0)
 
 BASE_VERSION_STR = '.'.join([str(x) for x in VERSION[:3]])
@@ -15,3 +17,47 @@ VERSION_STR = {
     'alpha': BASE_VERSION_STR + 'a' + str(VERSION[4]),
     'rc': BASE_VERSION_STR + 'rc' + str(VERSION[4]),
 }[VERSION[3]]
+
+# Build-state information that assumes the package has already been installed.
+# Can be used by device servers to get more detailed build info for build-state
+# informs.
+
+def construct_package_build_info(package, version):
+    """Construct a base build info tuple.
+
+    Arguments
+    ---------
+
+    package : str
+        name of the package to get build info for
+    version : tuple
+        The static version tuple containing (major, minor, point). Only the
+        first three entries are used
+
+    Returns
+    -------
+    build_info : tuple of (major, minor, release)
+        The base build information.
+    """
+
+    def get_git_rev(dist):
+        # See if there is a git version string
+        git_offs = dist.version.find('git-')
+        if git_offs < 0:
+            return None                   # no git info found
+        return dist.version[git_offs:]
+
+    try:
+        dist = pkg_resources.get_distribution(package)
+        # ver needs to be a list since tuples in Python <= 2.5 don't have
+        # a .index method.
+        ver = list(dist.parsed_version)
+        # Check if we have a git revision
+        rev = get_git_rev(dist)
+        if rev is None:
+            # Else try and get an SVN revision
+            rev = "r%d" % int(ver[ver.index("*r")+1])
+    except (pkg_resources.DistributionNotFound, ValueError, IndexError, TypeError):
+        rev = "unknown"
+
+    return version[:2] + (rev,)
