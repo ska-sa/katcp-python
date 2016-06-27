2521be56922b765e787fa9fec32500f35ec5d2fe
Charles de Villiers <charles@ska.ac.za>
2016-06-27 12:46:38 +0200

    Thread naming; wait_until_sensors_equal() method

--- katcp/server.py
+++ katcp/server.py
@@ -782,7 +782,6 @@ class ClientRequestConnection(object):
 class MessageHandlerThread(object):
     """Provides backwards compatibility for server expecting its own thread."""
     def __init__(self, handler, log_inform_formatter, logger=log):
-        import ipdb; ipdb.set_trace()
         self.handler = handler
         try:
             owner = handler.im_self.name
@@ -861,7 +860,6 @@ class MessageHandlerThread(object):
             self._logger.info('Message handler thread stopped.')
 
     def start(self, timeout=None):
-        import ipdb; ipdb.set_trace()
         if self._thread and self._thread.isAlive():
             raise RuntimeError('Cannot start since thread is already running')
         self._thread = threading.Thread(target=self.run, name=self.name)
