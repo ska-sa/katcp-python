ff25b3943014b79673402f6e59977d29df5ba56a
Francois Joubert <fjoubert@ska.ac.za>
2016-07-21 11:58:52 +0200

    removed unused deps

--- setup.py
+++ setup.py
@@ -44,8 +44,6 @@ setup(
     ],
     # run tests and install these requirements with python setup.py nosetests
     tests_require=[
-        "linecache2",
-        "traceback2",
         "unittest2",
         "nose",
         "mock",
