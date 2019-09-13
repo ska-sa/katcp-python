b9190a5ad9779d35544d1abed96d3ab07eefb70d
Anele Makhaba <31302703+amakhaba@users.noreply.github.com>
2019-09-13 16:31:22 +0200

    Fix Licensing (#216)
    * Fix Licencing
    * Fix and sort imports

--- bench/results.py
+++ bench/results.py
@@ -1,8 +1,7 @@
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
 # Copyright 2009 National Research Foundation (South African Radio Astronomy Observatory)
 # BSD license - see LICENSE for details
+
+from __future__ import absolute_import, division, print_function
 
 import math
 import json
