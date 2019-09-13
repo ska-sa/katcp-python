427ad477cdc309a242965671d18b4f09b369ccef
Anele Makhaba <amakhaba@ska.ac.za>
2019-09-13 16:03:51 +0200

    Fix Licencing

--- bench/util.py
+++ bench/util.py
@@ -1,8 +1,9 @@
+# Copyright 2009 National Research Foundation (South African Radio Astronomy Observatory)
+# BSD license - see LICENSE for details
+
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-# Copyright 2009 National Research Foundation (South African Radio Astronomy Observatory)
-# BSD license - see LICENSE for details
 
 from optparse import OptionParser
 
