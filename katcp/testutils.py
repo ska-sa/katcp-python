21ac009279fc91f536cc27a5898f54cbeb8ea56d
tockards <toufeeq.ockards+gitlab@gmail.com>
2020-04-30 11:22:39 +0200

    cast `default_request_names` to bytes

--- katcp/testutils.py
+++ katcp/testutils.py
@@ -717,12 +717,8 @@ class BlockingTestClient(client.BlockingClient):
         expected_set = set(expected_requests)
 
         if exclude_defaults:
-            default_request_names = set(DeviceServer._request_handlers.keys())
-            bytes_str_default_request_names = set()
-            for name in default_request_names:
-                bytes_str_default_request_names.add(native_str_to_bytes(name))
-
-            default_request_names = bytes_str_default_request_names
+            default_request_names = {native_str_to_bytes(name)
+                                     for name in DeviceServer._request_handlers.keys()}
             if not descriptions:
                 got_set = got_set - default_request_names
             else:
