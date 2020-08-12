00d12f23466217e4456aba46608f489ba2f88bcf
tockards <toufeeq.ockards+gitlab@gmail.com>
2020-08-12 15:57:04 +0200

    coerce other lists to native string lists.

--- katcp/testutils.py
+++ katcp/testutils.py
@@ -793,7 +793,8 @@ class BlockingTestClient(client.BlockingClient):
         informs_args_equal = kwargs.get("informs_args_equal")
 
         if args_echo:
-            args_equal = [str(p) for p in params]
+            args_equal = list_to_native_strings(params)
+
 
         if args_equal is not None:
             msg = ("Expected reply to request '%s' called with parameters %r "
@@ -818,8 +819,9 @@ class BlockingTestClient(client.BlockingClient):
             self.test.assertEqual(len(informs), informs_count, msg)
 
         if informs_args_equal is not None:
-            expected_inform_args = list(informs_args_equal)
-            actual_inform_args = [inform.arguments for inform in informs]
+            informs_args_equal = list_to_native_strings(list(informs_args_equal))
+            expected_inform_args = informs_args_equal
+            actual_inform_args = [list_to_native_strings(inform.arguments) for inform in informs]
             msg = ("Expected inform arguments {expected_inform_args} in reply to request "
                    "{requestname!r} called with parameters {params!r}, "
                    "but received {actual_inform_args!r}.".format(**locals()))
@@ -2080,3 +2082,13 @@ class TimewarpAsyncTestCaseTimeAdvancer(threading.Thread):
         yield self.test_instance.set_ioloop_time(
             self.test_instance.ioloop_time + self.quantum)
         future.set_result(None)
+
+
+def list_to_native_strings(list_to_coerce):
+    coerced_list = []
+    for item in list_to_coerce:
+        if is_bytes(item):
+            coerced_list.append(ensure_native_str(item))
+        else:
+            coerced_list.append(str(item))
+    return coerced_list
