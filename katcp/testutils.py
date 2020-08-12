2f8d8aa21ae82f5ef565506021235ad90e41d1d6
tockards <toufeeq.ockards+gitlab@gmail.com>
2020-08-12 16:46:04 +0200

    code cleanup

--- katcp/testutils.py
+++ katcp/testutils.py
@@ -820,8 +820,7 @@ class BlockingTestClient(client.BlockingClient):
             self.test.assertEqual(len(informs), informs_count, msg)
 
         if informs_args_equal is not None:
-            informs_args_equal = list_to_native_strings(list(informs_args_equal))
-            expected_inform_args = informs_args_equal
+            expected_inform_args = list_to_native_strings(list(informs_args_equal))
             actual_inform_args = [list_to_native_strings(inform.arguments) for inform in informs]
             msg = ("Expected inform arguments {expected_inform_args} in reply to request "
                    "{requestname!r} called with parameters {params!r}, "
