a43a81d6d6a4ad9ced80c644f78d7aa5216c8427
tockards <toufeeq.ockards+gitlab@gmail.com>
2020-08-19 10:49:58 +0200

    correct copyright from copy paste

--- katcp/test/test_compat.py
+++ katcp/test/test_compat.py
@@ -1,7 +1,7 @@
 # test_server.py
 # -*- coding: utf8 -*-
 # vim:fileencoding=utf8 ai ts=4 sts=4 et sw=4
-# Copyright 2009 National Research Foundation (South African Radio Astronomy Observatory)
+# Copyright 2020 National Research Foundation (South African Radio Astronomy Observatory)
 # BSD license - see LICENSE for details
 
 """Tests for the server module."""
